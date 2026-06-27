a653b2009b605396f6c0ed4a9ececb59c7aeabed
Francisco <franciscojosedemelolira@gmail.com>
2026-06-27 03:29:56 -0300

    versão 1.1

--- Test externo/banco.py
+++ Test externo/banco.py
@@ -1,30 +1,41 @@
 contas = [
-{"nome":"kiko",
+{"cpf": 1020,
+"limitesaque": 0,
+"limitedeposito": 0,
+"extrato": [],
+"info_conta": {
+"data_criação": "27/06/2026",
+"ultimo_login": "",
+"quantida_login": 0,
+"nome":"kiko",
 "senha": 10,
 "saldo": 1000,
-"extrato": {
-    "valor_transferido": 0,
-    "pessoa_transferencia": [],
-    "valor_recebido": 0,
-    "pessoa_recebido": [],
-    "valor_sacado": 0 ,
-    "valor_depositado": 0}},
-{"nome":"mario",
+}},
+{"cpf": 1030,
+"limitesaque": 0,
+"limitedeposito": 0,
+"extrato": [],
+"info_conta": {
+"data_criação": "27/06/2026",
+"ultimo_login": "",
+"quantida_login": 0,
+"nome":"mario",
 "senha": 20,
 "saldo": 3000,
-"extrato": {
-    "valor_transferido": 0,
-    "pessoa_transferencia": [],
-    "valor_recebido": 0,
-    "pessoa_recebido": [],
-    "valor_sacado": 0 ,
-    "valor_depositado": 0}}]
+
+
+}
+}]
 
 conta_logada = None
+from datetime import datetime
 
 def menu():
     while True:
         while True:
+            print(f"""
+        ===================
+        existem {len(contas)} usuarios cadastrados""")
             print("""
         ======= MENU =======
         1 - Criar conta
@@ -58,9 +69,12 @@ def criar_conta(): # criando conta
     global contas
     while True:
             usuario_existe = False
+            cpf = int(input("digite seu cpf:"))
             nome = input("digite seu nome:")
+
             while True: # verificar usuario igual
                 while True:# verificação de senha igual
+
                     try:
                         senha = int(input("digite sua senha(somente numeros):"))
                     except:
@@ -69,14 +83,24 @@ def criar_conta(): # criando conta
                         senhateste = int(input("digite sua senha novamente:"))
                     except:
                         print("senha deve ser somente numero")
+
                     if senha == senhateste:
                         for usuario in contas: 
-                            if usuario["nome"] == nome:
+                            if usuario["cpf"] == cpf:
                                usuario_existe = True
+
                         if usuario_existe == True:
-                            print("ja existe um usuario com este nome")
+                            print("ja existe um usuario com este cpf")
+
                         elif usuario_existe == False:
-                            contas.append({"nome":nome,"senha": senha, "saldo": 0, "extrato": {"valor_transferido": 0, "pessoa_transferencia": [], "pessoa_recebido": [], "valor_recebido": 0, "valor_sacado": 0 , "valor_depositado": 0}})
+                            contas.append({"cpf": cpf ,"limitesaque": 0,
+                            "limitedeposito": 0, "extrato": [],
+                            "info_conta": {
+                            "data_criação": datetime.now().strftime("%d/%m/%Y"),
+                            "quantida_login": 0,
+                            "nome":nome,"senha": senha,
+                            "saldo": 0,
+                            }})
 
                             print("conta criada com sucesso!")
                             break
@@ -89,22 +113,25 @@ def criar_conta(): # criando conta
     return
 def lista_usuarios():
     for usuario in contas:
-        print(usuario["nome"])
+        print(usuario["info_conta"]["nome"])
+
     return
 def entrar_conta():
     global conta_logada
     while True:
         usuario_existe = False
-        nome = input("digite seu nome:")
+        cpf = int(input("digite seu cpf:"))
         senha = int(input("digite sua senha:"))
         for usuario_logado in contas:
-            if usuario_logado["nome"] == nome and usuario_logado["senha"] == senha:
+            if usuario_logado["cpf"] == cpf and usuario_logado["info_conta"]["senha"] == senha:
                 usuario_existe = True
                 break
                
         if usuario_existe == True:
 
             conta_logada = usuario_logado
+            conta_logada["info_conta"]["quantida_login"] += 1
+            conta_logada["info_conta"]["ultimo_login"] = datetime.now().strftime("%d/%m/%y")
             print(f"você logou em {usuario_logado}")
             break
         else:
@@ -117,46 +144,79 @@ def opção_conta():
         escolha = int(input("""
     === opção de conta ===
     1 - excluir conta
-    2 - alterar senha
-    3 - extrato 
-    4 - sair 
+    2 - alterar nome
+    3 - alterar senha
+    4 - extrato 
+    5 - info de conta
+    6 - sair 
     ======================
      >>>"""))
         if escolha == 1: #excluir conta
-            escolha = int(input("""
-            você reamente deseja excluir sua conta?
-            1 - sim
-            2 - não 
-             >>>"""))
-            if escolha == 1:
-                print("você é louco!")
-                contas.remove(conta_logada)
-                return
+            excluir_conta()
+            return
 
-        elif escolha == 2: #alterar senha
-            while True:
-                nova_senha1 = int(input("digite sua nova senha:")) 
-                nova_senha2 = int(input("digite sua nova senha novamente:")) 
-                if nova_senha1 != nova_senha2:
-                    print("as duas senha devem ser iguais!")
-                else:
-                    conta_logada["senha"] = nova_senha1
-                    return
+        elif escolha == 2: # alterar nome
+            alterar_nome()
+            return
 
-        elif escolha == 3: # extrato
-            for item in conta_logada["extrato"].items():
+        elif escolha == 3: #alterar senha
+            alterar_senha()
+            return
+
+        elif escolha == 4: # extrato
+            for item in conta_logada["extrato"]:
                 print(item)
             return
-            
-        elif escolha == 4: # sair
+        elif escolha == 5: # ver informações da conta
+            for item in conta_logada["info_conta"].items():
+                print(item)
+        elif escolha == 6: # sair
             print("saindo da conta")
             conta_logada = None
         return
+def excluir_conta():
+
+    escolha = int(input("""
+    você reamente deseja excluir sua conta?
+    1 - sim
+    2 - não 
+    >>>"""))
+    if escolha == 1:
+        print("você é louco!")
+        contas.remove(conta_logada)
+    return
+def alterar_nome():
+    while True:
+        novo_nome1 = input("digite seu novo nome:")
+        novo_nome2 = input("digite seu novo nome novamente:")
+
+        if novo_nome2 != novo_nome1:
+            print("os dois nomes devem ser iguais!")
+
+        else:
+            conta_logada["info_conta"]["nome"] = novo_nome1
+            return
+def alterar_senha():
+    while True:
+        nova_senha1 = int(input("digite sua nova senha:")) 
+        nova_senha2 = int(input("digite sua nova senha novamente:")) 
+
+        if nova_senha1 != nova_senha2:
+            print("as duas senha devem ser iguais!")
+
+        else:
+            conta_logada["info_conta"]["senha"] = nova_senha1
+            return
 
 def menu_bancario():
     global conta_logada
     while True:
-        escolha = int(input("""
+        escolha = int(input(f"""
+        ==== MINHA CONTA ====
+        nome: {conta_logada["info_conta"]["nome"]}
+        saldo: {conta_logada["info_conta"]["saldo"]}
+        senha: {conta_logada["info_conta"]["senha"] }
+        numero de logins: {conta_logada["info_conta"]["quantida_login"]}
         === MENU BANCARIO ===
         1 - consultar saldo
         2 - depositar
@@ -182,57 +242,106 @@ def menu_bancario():
 
             return
 def consultar_saldo():
-    print(f"seu saldo atual é : {conta_logada["saldo"]}")
+    type = "consulta"
+
+    print(f"seu saldo atual é : {conta_logada["info_conta"]["saldo"]}")
+
+    conta_logada["extrato"].append({
+    "tipo": type,
+    "data": datetime.now().strftime("%d/%m/%y"),
+    "hora": datetime.now().strftime("%H/%M/%S")
+    
+    })
     return
 def depositar():
-    deposito = int(input("qual valor você deseja depositar?:"))
-    if deposito < 0:
-        print("você não pode depositar valor negativo")
+    type = "depositar"
+    if conta_logada["limitedeposito"] > 3:
+        print("você ja alcançou seu limite de depositos")
+        return
+
     else:
-        conta_logada["extrato"]["valor_depositado"] += deposito
-        conta_logada["saldo"] += deposito
+        deposito = int(input("qual valor você deseja depositar?:"))
+
+        if deposito < 0:
+            print("você não pode depositar valor negativo")
+
+        else:
+            conta_logada["extrato"].append({   
+    "tipo": type,
+    "data": datetime.now().strftime("%d/%m/%y"),
+    "hora": datetime.now().strftime("%H/%M/%S"),
+    "valor": deposito
+    })
+            conta_logada["info_conta"]["saldo"] += deposito
+            conta_logada["limitedeposito"] += 1
     return
 def sacar():
-    saque = int(input("digite o quanto quer sacar:"))
-
-    if saque > conta_logada["saldo"]:
-        print("você não possui esse valor em conta")
-
-    elif saque < 0:
-        print("você não pode sacar valor negativo")
-
+    type = "saque"
+    if conta_logada["limitesaque"] > 3:
+        print("você ja alcançou seu limite de saques")
+        return
     else:
-        conta_logada["extrato"]["valor_sacado"] += saque
-        conta_logada["saldo"] -= saque
+        saque = int(input("digite o quanto quer sacar:"))
+
+        if saque > conta_logada["info_conta"]["saldo"]:
+            print("você não possui esse valor em conta")
+
+        elif saque < 0:
+            print("você não pode sacar valor negativo")
+
+        else:
+            conta_logada["extrato"].append({
+    "tipo": type,
+    "data": datetime.now().strftime("%d/%m/%y"),
+    "hora": datetime.now().strftime("%H/%M/%S"),
+    "valor": - saque
+            })
+            conta_logada["info_conta"]["saldo"] -= saque
+            conta_logada["limitesaque"] += 1
     return
 def transferir():
+    type = "transferencia"
     global contas
     usuario_existe = False
-    destinatario = input("digite para quem deseja transferir:")
+    destinatario = int(input("digite o cpf de quem deseja transferir:"))
     for usuario_destinatario in contas:
-        if usuario_destinatario["nome"] == destinatario:
+        if usuario_destinatario["cpf"] == destinatario:
             usuario_existe = True
 
-    if usuario_destinatario["nome"] == conta_logada["nome"]:
+    if usuario_destinatario["cpf"] == conta_logada["cpf"]:
         print("você não pode transferir para si mesmo")
 
     if usuario_existe == True :
         destinatario = usuario_destinatario
         print("usuario existente")
         valor = int(input("quanto deseja transferir para esse pessoa?:"))
+
         if valor < 0:
             print("você não pode transferir valor negativo")
-        elif valor > conta_logada["saldo"]:
+
+        elif valor > conta_logada["info_conta"]["saldo"]:
             print("você não possui saldo")
             return
 
         else:
-            conta_logada["extrato"]["pessoa_transferencia"].append(destinatario["nome"])
-            conta_logada["extrato"]["valor_transferido"] += valor
-            destinatario["extrato"]["pessoa_recebido"].append(conta_logada["nome"])
-            destinatario["extrato"]["valor_recebido"] += valor 
-            conta_logada["saldo"] -= valor
-            destinatario["saldo"] += valor
+            conta_logada["extrato"].append({
+    "tipo": type,
+    "data": datetime.now().strftime("%d/%m/%y"),
+    "hora": datetime.now().strftime("%H/%M/%S"),
+    "valor": - valor,
+    "pessoa": destinatario["info_conta"]["nome"]
+            })
+            conta_logada["info_conta"]["saldo"] -= valor
+            destinatario["info_conta"]["saldo"] += valor
+            destinatario["extrato"].append({
+    "tipo": type,
+    "data": datetime.now().strftime("%d/%m/%y"),
+    "hora": datetime.now().strftime("%H/%M/%S"),
+    "valor": + valor,
+    "pessoa": conta_logada["info_conta"]["nome"]
+
+            })
+
         
     else:
         print("usuario inexistente")
